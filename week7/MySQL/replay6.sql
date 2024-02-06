923ba08adc9b765f23cda28b28316068525a0779
hwji <knjhw@daum.net>
2024-02-06 23:45:27 +0900

    쿼리문 복습

---
+++ week7/MySQL/replay6.sql
@@ -0,0 +1,20 @@
+CREATE TABLE customer (
+    id VARCHAR(10) NOT NULL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    birthday DATE NOT NULL
+);
+
+INSERT INTO customer (id, name, birthday) VALUES
+('aaa', '홍길동', '1990-03-17'),
+('bbb', '성춘향', '1992-02-06'),
+('ccc', '이몽룡', '1990-05-13'),
+
+CREATE TABLE orderlist (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    customer_id VARCHAR(10) NOT NULL,
+    product_name VARCHAR(20) NOT NULL,
+    quantity INT,
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+INSERT INTO orderlist (customer_id, product_name, quantity) VALUES
