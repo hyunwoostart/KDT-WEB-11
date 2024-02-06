208800409f4b9718c4b5b348949cb0b46b600e90
hwji <knjhw@daum.net>
2024-02-06 23:57:36 +0900

    실전문제 복습

--- week7/MySQL/replay.sql
+++ week7/MySQL/replay.sql
@@ -1,20 +1,28 @@
-CREATE TABLE customer (
+CREATE TABLE users (
     id VARCHAR(10) NOT NULL PRIMARY KEY,
-    name VARCHAR(10) NOT NULL,
-    birthday DATE NOT NULL
+    pw VARCHAR(20) NOT NULL,
+    name VARCHAR(5), NOT NULL,
+    gender ENUM('F', 'M', '') DEFAULT '',
+    birthday DATE NOT NULL,
+    age INT(3) NOT NULL DEFAULT 0;
 );
 
-INSERT INTO customer (id, name, birthday) VALUES
-('aaa', '홍길동', '1990-03-17'),
-('bbb', '성춘향', '1992-02-06'),
-('ccc', '이몽룡', '1991-05-13'),
+INSERT INTO users (id, pw, name, gender, birthday, age) VALUES
+('hong1234', '804bkg', '홍길동', 'M', '1990-01-31', 33),
+('sexysung', '87awjkdf', '성춘향', 'M', '1992-03-31', 31),
+('power70', 'qwur8sda', '변사또', 'M', '1970-05-02', 53),
+('hanjo', 'jk48fn4', '한조', 'M', '1984-10-18', 39),
+('widowmaker', '38ewifg3', '위도우', 'M', '1976-06-27', 47),
+('dvadva', 'k3f3ah', '송하나', 'M', '2001-06-03', 22),
+('jungkrat', '804bkg', '정크랫', 'M', '1999-11-11', 22);
 
-CREATE TABLE orderlist (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    customer_id VARCHAR(10) NOT NULL,
-    product_name VARCHAR(20) NOT NULL,
-    quantity INT,
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
-);
-
-INSERT INTO orderlist (customer_id, product_name, quantity) VALUES
+SELECT * FROM users ORDER BY birthday ASC;
+SELECT * FROM users WHERE gender = 'M' ORDER BY name DESC;
+SELECT id, name FROM users WHERE birthday LIKE '199%'
+SELECT * FROM users WHERE birthday LIKE '%-06-%' ORDER BY birthday ASC;
+SELECT * FROM users WHERE gender = 'M' AND birthday LIKE '197%';
+SELECT * FROM users ORDER BY age DESC LIMIT 3;
+SELECT * FROM users WHERE age BETWEEN 25 AND 50;
+SELECT * FROM users WHERE age >= 25 AND age <= 50;
+UPDATE users SET pw = '12345678' WHERE id = 'hong1234';
+DELETE FROM users id = 'jungkrat';
