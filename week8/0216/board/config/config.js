37e016b3d2488be44fd75dd37cd6d3e6cdbe4e63
hwji <knjhw@daum.net>
2024-02-16 13:28:36 +0900

    아이디 저장 로그인/ jwt 개념 정리

--- week8/0216/board/config/config.js
+++ week8/0216/board/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.USERNAME,
+        username: process.env.USERME,
         password: process.env.PASSWORD,
         database: process.env.DATABASE,
         host: process.env.HOST,
