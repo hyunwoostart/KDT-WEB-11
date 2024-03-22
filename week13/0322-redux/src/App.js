a1afc8acdcf0b1aa27df35de04c31697745573ac
hwji <knjhw@daum.net>
2024-03-22 13:38:53 +0900

    리액트 redux

---
+++ week13/0322-redux/src/App.js
@@ -0,0 +1,11 @@
+import { BankContainer } from './containers/BankContainers'
+
+function App() {
+  return (
+    <div className="App">
+      <BankContainer />
+    </div>
+  )
+}
+
+export default App
