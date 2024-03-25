80e63fa8f2d5776ee463831c82cec77478a0dc37
hwji <knjhw@daum.net>
2024-03-25 12:30:39 +0900

    타입스크립트로의 변환

---
+++ week14/0325-typescript-react/src/components/PostItem.tsx
@@ -0,0 +1,19 @@
+import { Post } from '../types';
+
+interface Props {
+  post: Post;
+}
+
+const PostItem = ({ post }: Props) => {
+  return (
+    <div className='PostItem'>
+      <div>
+        <span className='id'>No. {post.id}</span>
+        <span className='title'>- {post.title}</span>
+      </div>
+      <p className='body'>{post.body.slice(0, 120)}...</p>
+    </div>
+  );
+};
+
+export default PostItem;
