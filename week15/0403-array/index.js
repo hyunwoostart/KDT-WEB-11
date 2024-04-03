364bcf785e8e418b1d289b2904404ba1ee2c662a
hwji <knjhw@daum.net>
2024-04-03 23:46:43 +0900

    배열 함수

---
+++ week15/0403-array/index.js
@@ -0,0 +1,16 @@
+const fruits = [
+    { name: '🍊', price: 300 },
+    { name: '🍌', price: 700 },
+    { name: '🍎', price: 500 },
+    { name: '🍋', price: 1000 },
+];
+
+
+const doesExist = fruits.some((fruit) => {
+    if(fruit.price > 600) {
+        return true
+    }
+    return false
+})
+
+console.log(doesExist);
